864b56e77c0260f591cced8aee8a836d6db9542b
Arthur 'sevak' Costa Serra Negra <163688467+sevak19@users.noreply.github.com>
2024-06-26 19:53:38 -0300

    update

--- assets/js/jsrepo.js
+++ assets/js/jsrepo.js
@@ -22,12 +22,15 @@ function carregarInformacoesRepositorio() {
         .then(data => {
             console.log(data.bio);
             document.getElementById('titulo').innerHTML = `
-            Repositório: ${data.name}
-            `
+            Repositório: ${data.name}`
             document.getElementById('descricao').textContent = data.description;
             document.getElementById('dataCriacao').textContent = data.created_at;
             document.getElementById('linguagem').textContent = data.language;
-            document.getElementById('linkAcesso').innerHTML = `<a href="${data.html_url}" target="_blank">${data.html_url}</a>`;
+            document.getElementById('linkAcesso').innerHTML = `
+            <a href="${data.html_url}" target="_blank">${data.html_url}</a>`;
+            document.getElementById('simbolos').innerHTML = `
+            <i class="ph-bold ph-star"></i> ${data.stargazers_count}
+            <i class="ph ph-user"></i> ${data.forks_count}`
         })
         .catch(error => console.error('Erro ao carregar informações do repositório:', error));
 }
